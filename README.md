5a0602a151cf78520fe171f45838e7a63886445d
Shun Ueda <hasundue@gmail.com>
2017-10-04 11:25:14 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # UnitfulNLopt
+
+This package enables NLopt to handle the Quantity types implemented in [Unitful.jl](https://github.com/ajkeller34/Unitful.jl).
+
+Note that this package has limited functionalities and is not well-tested so far.
+
+## Usage
+
+You initialize an optimization by defining UnitfulOpt object with information on units with variables and function values instead of defining Opt object:
+
+```julia
+opt = UnitfulOpt(:LN_SBPLX, N, unit(1u"m"), NoUnits)
+min_objective!(opt, f)
+```
+
+In this case, the variables of the target function f have m (meter) as their units and function values have no units. After that you can use the same functions as NLopt:
+
+```julia
+ftol_rel!(opt, 1e-6)
+xtol_abs!(opt, 1u"nm")
+maxeval!(opt, 100)
+...
+fmin, xmin, res = optimize(opt, x₀)
+```
+
+Currently, the following functions and commands are supported:
+
+- optimize
+- min_objective
+- algorithm
+- stopval
+- ftol_rel
+- ftol_abs
+- xtol_rel
+- xtol_abs
+- maxeval
+- maxtime
+- initial_step
--- src/UnitfulNLopt.jl
+++ src/UnitfulNLopt.jl
@@ -1,5 +1,67 @@
 module UnitfulNLopt
 
-# package code goes here
+using Unitful
+import Unitful: Quantity, Units
+import NLopt
+import NLopt: Opt
+
+export UnitfulOpt
+
+struct UnitfulOpt
+    opt::Opt
+    xunit::Units
+    funit::Units
+end
+
+function UnitfulOpt(s::Symbol, N::Int, xu::Units, fu::Units)
+    UnitfulOpt(Opt(s, N), xu, fu)
+end
+
+function NLopt.min_objective!(uopt::UnitfulOpt, f)
+    g(x,y) = ustrip(f(x*uopt.xunit, y*uopt.funit*uopt.xunit^-1))
+    NLopt.min_objective!(uopt.opt, g)
+end
+
+for f in (:optimize, :optimize!)
+    @eval function NLopt.$f(uopt::UnitfulOpt, x::AbstractVector)
+        fmin, xmin, res = NLopt.$f(uopt.opt, ustrip(x))
+        fmin*uopt.funit, xmin*uopt.xunit, res
+    end
+end
+
+for f in (:algorithm,)
+    @eval function NLopt.$f(uopt::UnitfulOpt)
+        NLopt.$f(uopt.opt)
+    end
+end
+
+fset(f::Symbol) = Symbol(f, '!')
+
+for f in (:ftol_rel, :xtol_rel, :maxeval, :maxtime)
+    @eval function NLopt.$f(uopt::UnitfulOpt)
+        NLopt.$f(uopt.opt)
+    end
+    @eval function NLopt.$(fset(f))(uopt::UnitfulOpt, x)
+        NLopt.$(fset(f))(uopt.opt, x)
+    end
+end
+
+for f in (:stopval, :ftol_abs)
+    @eval function NLopt.$f(uopt::UnitfulOpt)
+        NLopt.$f(uopt.opt) * uopt.funit
+    end
+    @eval function NLopt.$(fset(f))(uopt::UnitfulOpt, x)
+        NLopt.$(fset(f))(uopt.opt, ustrip(uconvert(uopt.funit, x)))
+    end
+end
+
+for f in (:xtol_abs, :lower_bounds, :upper_bounds, :initial_step)
+    @eval function NLopt.$f(uopt::UnitfulOpt)
+        NLopt.$f(uopt.opt) * uopt.xunit
+    end
+    @eval function NLopt.$(fset(f))(uopt::UnitfulOpt, x)
+        NLopt.$(fset(f))(uopt.opt, ustrip(uconvert(uopt.xunit, x)))
+    end
+end
 
 end # module
